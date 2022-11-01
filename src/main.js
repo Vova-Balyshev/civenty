4705db585c6e308046304957201e73047a307373
Volodya <balyshev.volodya@gmail.com>
2022-11-01 18:28:56 +0200

    mobile version developed

--- src/main.js
+++ src/main.js
@@ -1,1 +1,45 @@
-import './styles/style.scss';
+// import Swiper from 'swiper/bundle';
+
+// import 'swiper/css/bundle';
+// import './styles/style.scss';
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.header__nav-menu');
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  document.body.classList.toggle('body__lock-scroll');
+  navMenu.classList.toggle('active');
+}
+
+hamburger.addEventListener('click', mobileMenu);
+
+const navLink = document.querySelectorAll('.header__nav-link');
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
+
+function lockScroll() {
+  document.body.classList.toggle('lock-scroll');
+}
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'gorizontal',
+//   loop: true,
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   breakpoints: {
+
+//   },
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,43 @@ const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
-   output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-   },
-   devServer: {
+  devServer: {
     static: './dist',
   },
-   module: {
-      rules: [{
-         test:/\.(s*)css$/,
-         use: [
-            miniCss.loader,
-            'css-loader',
-            'sass-loader',
-         ]
-      }]
-   },
+  module: {
+    rules: [{
+      test:/\.(s*)css$/,
+      use: [
+        miniCss.loader,
+        'css-loader',
+        'sass-loader',
+      ]
+    },
+    {
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      use: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            disable: true, // webpack@2.x and newer
+          },
+        },
+      ],
+    }]
+  },
   //  optimization: {
-  //   runtimeChunk: 'single',
-  // },
-   plugins: [
+    //   runtimeChunk: 'single',
+    // },
+    plugins: [
       new miniCss({
-         filename: 'style.css',
+        filename: 'style.css',
       }),
-   ]
-};
+    ],
+    output: {
+       filename: 'bundle.js',
+       path: path.resolve(__dirname, 'dist')
+    },
+  };
